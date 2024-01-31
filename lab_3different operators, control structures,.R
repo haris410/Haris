6e8b8fc0299b3de9026a96d5d4d0d2fd34657c49
Mohammed Haris <141272429+haris410@users.noreply.github.com>
2024-01-31 19:21:25 +0530

    Add files via upload

---
+++ lab_3different operators, control structures,.R
@@ -0,0 +1,61 @@
+# Function with default argument values
+calculate_area <- function(shape = "circle", radius = 1, length = 1, width = 1) {
+  if (shape == "circle") {
+    area <- pi * radius^2
+  } else if (shape == "rectangle") {
+    area <- length * width
+  } else {
+    area <- 0
+  }
+  return(list(shape = shape, area = area))
+}
+
+
+# Calculate areas using the function
+circle_area <- calculate_area("circle", radius = 5)
+rect_area <- calculate_area("rectangle", length = 4, width = 6)
+default_area <- calculate_area()
+
+
+# Print the results
+cat("Circle Area:", circle_area$area, "for shape:",circle_area$shape, "\n")
+cat("Rectangle Area:", rect_area$area, "for shape:",rect_area$shape, "\n")
+cat("Default Area:", default_area$area, "for shape:",default_area$shape, "\n")
+
+
+# Conditional statements
+grade <- 85
+if (grade >= 90) {
+  cat("A\n")
+} else if (grade >= 80) {
+  cat("B\n")
+} else if (grade >= 70) {
+  cat("C\n")
+} else {
+  cat("F\n")
+}
+
+
+# Loop
+for (i in 1:5) {
+  cat("Iteration:", i, "\n")
+}
+
+# Logical operators
+is_sunny <- TRUE
+is_warm <-TRUE
+
+if (is_sunny && is_warm) {
+cat("It's a nice day!\n")
+}
+
+
+# Complex objects (list of lists)
+student1 <- list(name = "Alice", age = 25)
+student2 <- list(name = "Bob", age = 22)
+students <- list(student1, student2)
+
+
+# Accessing complex object elements
+cat("First student's name:", students[[1]]$name, "\n")
+cat("Second student's age:", students[[2]]$age, "\n")
---
+++ quick_sort.R
@@ -0,0 +1,19 @@
+#quick sort function
+
+quick_sort = function(arr)
+{
+  if (length(arr)<=1)
+  {
+    return(arr)
+    }
+  pivot = arr[1]
+  less = arr[arr < pivot]
+  equal = arr[arr == pivot]
+  greater = arr[arr>pivot]
+  return(c(quick_sort(less),equal,quick_sort(greater)))
+}
+
+#example
+unsorted_array = c(9,5,7,3)
+sorted_array = quick_sort(unsorted_array)
+cat("QUICK_SORT is in sorted Array:",sorted_array,"\n")
